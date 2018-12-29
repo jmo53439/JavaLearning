f24cb9ec9e6b501963582eab1b727e337dc175d8
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2018-12-29 14:50:31 -0600

    Create PhoneClient.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/observer/PhoneClient.java
@@ -0,0 +1,21 @@
+package com.jmlearning.designpatterns.behavioral.observer;
+
+public class PhoneClient extends Observer {
+
+    public PhoneClient(Subject subject) {
+
+        this.subject = subject;
+        subject.attach(this);
+    }
+
+    public void addMessage(String message) {
+
+        subject.setState(message + " - sent from phone");
+    }
+
+    @Override
+    void update() {
+
+        System.out.println("Phone Stream: " + subject.getState());
+    }
+}
