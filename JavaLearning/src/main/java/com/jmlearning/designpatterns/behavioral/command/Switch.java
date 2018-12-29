356ed81d4f679fdce7412424aa28343a4aa275cd
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2018-12-29 12:20:42 -0600

    Create Switch.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/command/Switch.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.behavioral.command;
+
+// invoker
+public class Switch {
+
+    public void storeAndExecute(Command command) {
+
+        command.execute();
+    }
+}
