0ccd5db5a342aa4b2c7a19d31a311ed37249a0ab
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2018-12-29 14:25:37 -0600

    Create Employee.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/memento/Employee.java
@@ -0,0 +1,56 @@
+package com.jmlearning.designpatterns.behavioral.memento;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+
+    private String name;
+    private String address;
+    private String phone;
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public void setAddress(String address) {
+
+        this.address = address;
+    }
+
+    public String getPhone() {
+
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+    }
+
+    public String toString() {
+
+        return name + " : " + phone;
+    }
+
+    public EmployeeMemento save() {
+
+        return new EmployeeMemento(name, phone);
+    }
+
+    public void revert(EmployeeMemento emp) {
+
+        this.name = emp.getName();
+        this.phone = emp.getPhone();
+    }
+}
