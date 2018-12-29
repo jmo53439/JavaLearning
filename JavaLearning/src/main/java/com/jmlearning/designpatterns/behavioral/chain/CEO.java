3a55224c96febecb71d355d6da85b59b999d7c50
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2018-12-29 11:38:11 -0600

    Create CEO.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/chain/CEO.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.behavioral.chain;
+
+public class CEO extends Handler {
+
+    @Override
+    public void handleRequest(Request request) {
+
+        System.out.println("CEOs can approve anything they want");
+    }
+}
