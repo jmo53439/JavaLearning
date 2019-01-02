2b8428c21dbc5fdfb29e903d46204b7139d392b6
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-02 15:50:54 -0600

    Create FanHighState.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/state/FanHighState.java
@@ -0,0 +1,23 @@
+package com.jmlearning.designpatterns.behavioral.state;
+
+public class FanHighState extends State {
+
+    private Fan fan;
+
+    public FanHighState(Fan fan) {
+
+        this.fan = fan;
+    }
+
+    @Override
+    public void handleRequest() {
+
+        System.out.println("Turning fan off...");
+        fan.setState(fan.getFanOffState());
+    }
+
+    public String toString() {
+
+        return "Fan is high";
+    }
+}
