9d5551efa8b3df5dcf4731c3774660de43c22a3a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-02 15:52:24 -0600

    Create StateDemo.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/state/StateDemo.java
@@ -0,0 +1,24 @@
+package com.jmlearning.designpatterns.behavioral.state;
+
+public class StateDemo {
+
+    public static void main(String[] args) {
+
+        Fan fan = new Fan();
+
+        System.out.println(fan);
+        fan.pullChain();
+
+        System.out.println(fan);
+        fan.pullChain();
+
+        System.out.println(fan);
+        fan.pullChain();
+
+        System.out.println(fan);
+        fan.pullChain();
+
+        System.out.println(fan);
+
+    }
+}
