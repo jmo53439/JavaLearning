f803330e3703be32322a7c9716afa2eb62bef714
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-03 16:34:48 -0600

    Create Fender.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/behavioral/visitor/visitorgood/Fender.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.behavioral.visitor.visitorgood;
+
+public class Fender implements AtvPart {
+
+    @Override
+    public void accept(AtvPartVisitor visitor) {
+
+        visitor.visit(this);
+    }
+}
