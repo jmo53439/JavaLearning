f4db3eb9d7b1b8d92e5923307cbd10de3a4e9d2a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 14:45:07 -0600

    Create BlueCircle.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape1badexample/BlueCircle.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape1badexample;
+
+public class BlueCircle extends Circle {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying blue color");
+    }
+}
