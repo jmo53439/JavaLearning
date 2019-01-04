443f26c23e8f1240a5fbb754480537c3ac968292
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 11:37:32 -0600

    Create EmployeeClient.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/adapter/EmployeeClient.java
@@ -0,0 +1,29 @@
+package com.jmlearning.designpatterns.structural.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeClient {
+
+    public List<Employee> getEmployeeList() {
+
+        List<Employee> employees = new ArrayList <>();
+
+        Employee employeeFromDB = new EmployeeDB("1234", "Joe", "Smith",
+                "joesmith@iamlearning.com");
+
+        employees.add(employeeFromDB);
+
+        // this will not work. this is where the adapter comes into play
+
+//    Employee employeeFromLdap = new EmployeeLdap("meow", "Smith", "Jane",
+//            "janesmith@iamlearning.com");
+
+        EmployeeLdap employeeFromLdap = new EmployeeLdap("meow", "Smith", "Jane",
+                "janesmith@iamlearning.com");
+
+        employees.add(new EmployeeAdapterLdap(employeeFromLdap));
+
+        return employees;
+    }
+}
