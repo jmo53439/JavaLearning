c6ff538921e2098c03f8ad4aa0e2c152b981f3df
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 15:01:42 -0600

    Create GreenSquare.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape1badexample/GreenSquare.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape1badexample;
+
+public class GreenSquare extends Square {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying green color");
+    }
+}
