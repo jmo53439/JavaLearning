1f3a5f826842d70593a400e9c3ae6fde0b3bdbe1
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 15:02:30 -0600

    Create Shape.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape1badexample/Shape.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape1badexample;
+
+public abstract class Shape {
+
+    public Shape() {
+
+    }
+
+    abstract public void applyColor();
+}
