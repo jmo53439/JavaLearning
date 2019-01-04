ee835dc5f0e405a60b242e3b0fd6b0f059d2748a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 15:01:11 -0600

    Create BlueSquare.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape1badexample/BlueSquare.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape1badexample;
+
+public class BlueSquare extends Square {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying blue color");
+    }
+}
