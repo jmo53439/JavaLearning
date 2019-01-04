bc54de2ef4e2600d26e11146b479d4261f290563
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 15:02:14 -0600

    Create RedSquare.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape1badexample/RedSquare.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape1badexample;
+
+public class RedSquare extends Square {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying red color");
+    }
+}
