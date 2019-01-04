eafefcad695857dadfa83dbb72a4144bdc15acb5
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-04 11:37:14 -0600

    Create EmployeeAdapterLdap.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/adapter/EmployeeAdapterLdap.java
@@ -0,0 +1,41 @@
+package com.jmlearning.designpatterns.structural.adapter;
+
+public class EmployeeAdapterLdap implements Employee {
+
+    private EmployeeLdap instance;
+
+    public EmployeeAdapterLdap(EmployeeLdap instance) {
+
+        this.instance = instance;
+    }
+
+    @Override
+    public String getId() {
+
+        return instance.getCn();
+    }
+
+    @Override
+    public String getFirstName() {
+
+        return instance.getGivenName();
+    }
+
+    @Override
+    public String getLastName() {
+
+        return instance.getSurname();
+    }
+
+    @Override
+    public String getEmail() {
+
+        return instance.getMail();
+    }
+
+    public String toString() {
+
+        return "ID: " + instance.getCn() + ", First Name: " + instance.getGivenName()
+                + ", Surname: " + instance.getSurname() + ", Email: " + instance.getMail();
+    }
+}
