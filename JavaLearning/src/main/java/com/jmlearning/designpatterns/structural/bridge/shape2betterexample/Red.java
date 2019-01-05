c24807ff3cabf0693a46f02ac9a191ef619bd4f9
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-05 11:05:27 -0600

    Create Red.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape2betterexample/Red.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape2betterexample;
+
+public class Red implements Color {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying red color");
+    }
+}
