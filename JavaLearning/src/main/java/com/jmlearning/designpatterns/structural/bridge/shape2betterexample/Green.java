9be1bdb8df11c6cc7b01c7d8b9106a0781fc36ab
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-05 11:05:09 -0600

    Create Green.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/bridge/shape2betterexample/Green.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.structural.bridge.shape2betterexample;
+
+public class Green implements Color {
+
+    @Override
+    public void applyColor() {
+
+        System.out.println("Applying green color");
+    }
+}
