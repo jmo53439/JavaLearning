302b58a60c0a8d794d219b463c11fa3d67597367
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-05 15:04:27 -0600

    Create Item.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/flyweight/Item.java
@@ -0,0 +1,16 @@
+package com.jmlearning.designpatterns.structural.flyweight;
+
+class Item {
+
+    private final String name;
+
+    public Item(String name) {
+
+        this.name = name;
+    }
+
+    public String toString() {
+
+        return name;
+    }
+}
