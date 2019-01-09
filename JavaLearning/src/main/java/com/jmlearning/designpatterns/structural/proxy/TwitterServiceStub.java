220fd5a5a16e8e8a1cc90f22e0cea8fb65db29c1
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-09 14:33:23 -0600

    Create TwitterServiceStub.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/structural/proxy/TwitterServiceStub.java
@@ -0,0 +1,14 @@
+package com.jmlearning.designpatterns.structural.proxy;
+
+public class TwitterServiceStub implements TwitterService {
+
+    @Override
+    public String getTimeline(String screenName) {
+        return "My timeline";
+    }
+
+    @Override
+    public void postToTimeline(String screenName, String message) {
+
+    }
+}
