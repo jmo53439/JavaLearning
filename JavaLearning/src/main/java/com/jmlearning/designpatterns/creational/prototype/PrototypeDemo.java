94584f7d2cfe68770e812b5d3cfa6753a569fcfd
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-10 14:34:03 -0600

    Create PrototypeDemo.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/creational/prototype/PrototypeDemo.java
@@ -0,0 +1,25 @@
+package com.jmlearning.designpatterns.creational.prototype;
+
+public class PrototypeDemo {
+
+    public static void main(String[] args) {
+
+        Registry registry = new Registry();
+
+        Movie movie = (Movie) registry.createItem("Movie");
+        movie.setTitle("Java Design Patterns");
+
+        System.out.println(movie);
+        System.out.println(movie.getRuntime());
+        System.out.println(movie.getTitle());
+        System.out.println(movie.getUrl());
+
+        Movie anotherMovie = (Movie) registry.createItem("Movie");
+        anotherMovie.setTitle("Wanna Learn Unity");
+
+        System.out.println(anotherMovie);
+        System.out.println(anotherMovie.getRuntime());
+        System.out.println(anotherMovie.getTitle());
+        System.out.println(anotherMovie.getUrl());
+    }
+}
