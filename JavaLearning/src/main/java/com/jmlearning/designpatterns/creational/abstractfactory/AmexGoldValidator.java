14d97d083ea11955e641c5ccf71a0d25c053428e
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-11 15:47:43 -0600

    Create AmexGoldValidator.java

---
+++ JavaLearning/src/main/java/com/jmlearning/designpatterns/creational/abstractfactory/AmexGoldValidator.java
@@ -0,0 +1,10 @@
+package com.jmlearning.designpatterns.creational.abstractfactory;
+
+public class AmexGoldValidator implements Validator {
+
+    @Override
+    public boolean isValid(CreditCard creditCard) {
+
+        return false;
+    }
+}
