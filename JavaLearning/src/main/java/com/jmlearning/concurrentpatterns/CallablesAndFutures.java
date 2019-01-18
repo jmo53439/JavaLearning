391e8c7884c2752e80142895555b4f5db13e60cb
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-01-18 11:09:53 -0600

    Create CallablesAndFutures.java

---
+++ JavaLearning/src/main/java/com/jmlearning/concurrentpatterns/CallablesAndFutures.java
@@ -0,0 +1,30 @@
+package com.jmlearning.concurrentpatterns;
+
+import java.util.concurrent.*;
+
+public class CallablesAndFutures {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        Callable<String> task = () -> {
+
+            Thread.sleep(300);
+            return "I am in thread " + Thread.currentThread().getName();
+//            throw new IllegalStateException("I throw an exception in thread " + Thread.currentThread().getName());
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(4);
+
+//        try {
+            for (int i = 0; i < 10; i++) {
+
+                Future <String> future = executor.submit(task);
+                System.out.println("I get: " + future.get());
+            }
+//        }
+//        finally {
+
+            executor.shutdown();
+//        }
+    }
+}
