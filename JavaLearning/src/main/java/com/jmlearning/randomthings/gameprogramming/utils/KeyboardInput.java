2e96cc9c69e69e316b1dcf2b74b743126f72d7cd
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-02-02 11:23:45 -0600

    Create KeyboardInput.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/utils/KeyboardInput.java
@@ -0,0 +1,65 @@
+package com.jmlearning.randomthings.gamingprogramming.utils;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInput implements KeyListener {
+
+    private boolean[] keys;
+    private int[] polled;
+
+    public KeyboardInput() {
+
+        keys = new boolean[256];
+        polled = new int[256];
+    }
+
+    public boolean keyDown(int keyCode) {
+
+        return polled[keyCode] > 0;
+    }
+
+    public boolean keyDownOnce(int keyCode) {
+
+        return polled[keyCode] == 1;
+    }
+
+    public synchronized void poll() {
+
+        for(int i = 0; i < keys.length; i++) {
+
+            if(keys[i]) {
+
+                polled[i]++;
+            }
+            else {
+
+                polled[i] = 0;
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+        // not used
+    }
+
+    @Override
+    public synchronized void keyPressed(KeyEvent e) {
+
+        int keyCode = e.getKeyCode();
+
+        if(keyCode >= 0 && keyCode < keys.length)
+            keys[keyCode] = true;
+    }
+
+    @Override
+    public synchronized void keyReleased(KeyEvent e) {
+
+        int keyCode = e.getKeyCode();
+
+        if(keyCode >= 0 && keyCode < keys.length)
+            keys[keyCode] = false;
+    }
+}
