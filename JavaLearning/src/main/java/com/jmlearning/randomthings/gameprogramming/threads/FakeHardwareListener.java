f40eb5f6e6b7f12db41e7381ee90cfb55b2c269c
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-02-22 14:34:12 -0600

    Create FakeHardwareListener.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/threads/FakeHardwareListener.java
@@ -0,0 +1,6 @@
+package com.jmlearning.randomthings.gamingprogramming.threads;
+
+public interface FakeHardwareListener {
+
+    public void event(FakeHardware source, FakeHardware.FakeHardwareEvent event);
+}
