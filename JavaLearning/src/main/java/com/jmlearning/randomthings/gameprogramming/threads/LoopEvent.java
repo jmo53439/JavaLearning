d0590e02c73408d968d6d2a2121a75bb50033c48
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-02-23 11:20:51 -0600

    Create LoopEvent.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/threads/LoopEvent.java
@@ -0,0 +1,137 @@
+package com.jmlearning.randomthings.gamingprogramming.threads;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class LoopEvent implements Runnable {
+
+    private enum State {
+
+        WAITING, RUNNING
+    }
+
+    private enum Event {
+
+        FIRE, RESTART, DONE
+    }
+
+    private BlockingQueue<Event> queue;
+    private BlockingHardware hardware;
+    private State currentState;
+    private Thread consumer;
+    private int ms;
+    private int slices;
+
+    public LoopEvent(int ms, int slices) {
+
+        this.ms = ms;
+        this.slices = slices;
+    }
+
+    public void initialize() {
+
+        hardware = new BlockingHardware("looper");
+        hardware.addListener(getListener());
+        queue = new LinkedBlockingQueue <>();
+        currentState = State.WAITING;
+        consumer = new Thread(this);
+        consumer.start();
+    }
+
+    public void fire() {
+
+        try {
+
+            queue.put(Event.FIRE);
+        }
+        catch(InterruptedException e) {
+
+        }
+    }
+
+    public void done() {
+
+        try {
+
+            queue.put(Event.DONE);
+        }
+        catch(InterruptedException e) {
+
+        }
+    }
+
+    public void shutDown() {
+
+        Thread temp = consumer;
+        consumer = null;
+
+        try {
+
+            queue.put(Event.DONE);
+            temp.join(10000L);
+            System.out.println("Loop Shutdown");
+        }
+        catch(InterruptedException ex) {
+
+        }
+    }
+
+    @Override
+    public void run() {
+
+        while(Thread.currentThread() == consumer) {
+
+            try {
+
+                processEvent(queue.take());
+            }
+            catch(InterruptedException e) {
+
+            }
+        }
+    }
+
+    private void processEvent(Event event) throws InterruptedException {
+
+        System.out.println("Got " + event + " event");
+
+        if(currentState == State.WAITING) {
+
+            if(event == Event.FIRE) {
+
+                hardware.turnOn();
+                hardware.start(ms, slices);
+                currentState = State.RUNNING;
+            }
+        }
+        else if(currentState == State.RUNNING) {
+
+            if(event == Event.RESTART) {
+
+                hardware.start(ms, slices);
+                currentState = State.RUNNING;
+            }
+
+            if(event == Event.DONE) {
+
+                hardware.stop();
+                hardware.turnOff();
+                currentState = State.WAITING;
+            }
+        }
+    }
+
+    private BlockingHardwareListener getListener() {
+
+        return () -> {
+
+            try {
+
+                queue.put(Event.RESTART);
+            }
+            catch(InterruptedException e) {
+
+            }
+        };
+    }
+}
