1f8511569bff290423199b816db61fe6597bc2fa
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-02-25 11:43:53 -0600

    Create SoundException.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/sound/SoundException.java
@@ -0,0 +1,14 @@
+package com.jmlearning.randomthings.gamingprogramming.sound;
+
+public class SoundException extends RuntimeException {
+
+    public SoundException(String message) {
+        
+        super(message);
+    }
+    
+    public SoundException(String message, Throwable cause) {
+        
+        super(message, cause);
+    }
+}
