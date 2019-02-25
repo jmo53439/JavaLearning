1d43c079e8af38b37845dbd5ff52b6f27372b0b9
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-02-25 08:31:25 -0600

    Create AudioStream.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/sound/AudioStream.java
@@ -0,0 +1,149 @@
+package com.jmlearning.randomthings.gamingprogramming.sound;
+
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public abstract class AudioStream implements LineListener {
+
+    public static final int LOOP_CONTINUOUSLY = -1;
+    protected final Lock lock = new ReentrantLock();
+    protected final Condition condition = lock.newCondition();
+    protected volatile boolean open = false;
+    protected volatile boolean started = false;
+    protected FloatControl gainControl;
+    protected FloatControl panControl;
+    protected byte[] soundData;
+    private List<BlockingAudioListener> listeners =
+            Collections.synchronizedList(new ArrayList <>());
+
+    public AudioStream(byte[] soundData) {
+
+        this.soundData = soundData;
+    }
+
+    public abstract void open();
+    public abstract void close();
+    public abstract void start();
+    public abstract void loop(int count);
+    public abstract void restart();
+    public abstract void stop();
+
+    public boolean addListener(BlockingAudioListener listener) {
+
+        return listeners.add(listener);
+    }
+
+    protected void fireTaskFinished() {
+
+        synchronized(listeners) {
+
+            for(BlockingAudioListener listener : listeners) {
+
+                listener.audioFinished();
+            }
+        }
+    }
+
+    @Override
+    public void update(LineEvent event) {
+
+        boolean wasStarted = started;
+        lock.lock();
+
+        try {
+
+            if(event.getType() == LineEvent.Type.OPEN) {
+
+                open = true;
+            }
+            else if(event.getType() == LineEvent.Type.CLOSE) {
+
+                open = false;
+            }
+            else if(event.getType() == LineEvent.Type.START) {
+
+                started = true;
+            }
+            else if(event.getType() == LineEvent.Type.STOP) {
+
+                started = false;
+            }
+
+            condition.signalAll();
+        }
+        finally {
+
+            lock.unlock();
+        }
+
+        if(wasStarted && !started) {
+
+            fireTaskFinished();
+        }
+    }
+
+    public void clearControls() {
+
+        gainControl = null;
+        panControl = null;
+    }
+
+    public void createControls(Line line) {
+
+        if(line.isControlSupported(FloatControl.Type.MASTER_GAIN)) {
+
+            gainControl = (FloatControl) line.getControl(
+                    FloatControl.Type.MASTER_GAIN);
+        }
+
+        if(line.isControlSupported(FloatControl.Type.PAN)) {
+
+            panControl = (FloatControl) line.getControl(FloatControl.Type.PAN);
+        }
+    }
+
+    public boolean hasGainControl() {
+
+        return gainControl != null;
+    }
+
+    public void setGain(float fGain) {
+
+        if(hasGainControl())
+            gainControl.setValue(fGain);
+    }
+
+    public boolean hasPanControl() {
+
+        return panControl != null;
+    }
+
+    public void setPan(float pan) {
+
+        if(hasPanControl())
+            panControl.setValue(pan);
+    }
+
+    public float getGain() {
+
+        return hasGainControl() ? gainControl.getValue() : 0.0f;
+    }
+
+    public float getMinimum() {
+
+        return hasGainControl() ? gainControl.getMinimum() : 0.0f;
+    }
+
+    public float getMaximum() {
+
+        return hasGainControl() ? gainControl.getMaximum() : 0.0f;
+    }
+}
