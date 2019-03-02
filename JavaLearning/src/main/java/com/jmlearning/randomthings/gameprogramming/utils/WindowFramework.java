fc442ce9412e84369dd9e8381a1479f963b2879b
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-02 12:03:24 -0600

    Create WindowFramework.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/utils/WindowFramework.java
@@ -0,0 +1,72 @@
+package com.jmlearning.randomthings.gamingprogramming.utils;
+
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+public class WindowFramework extends GameFramework {
+
+    private Canvas canvas;
+
+    @Override
+    protected void createFramework() {
+        
+        canvas = new Canvas();
+        canvas.setBackground(appBackground);
+        canvas.setIgnoreRepaint(true);
+        getContentPane().add(canvas);
+        setLocationByPlatform(true);
+        
+        if(appMaintainRatio) {
+            
+            getContentPane().setBackground(appBorder);
+            setSize(appWidth, appHeight);
+            setLayout(null);
+            getContentPane().addComponentListener(new ComponentAdapter() {
+
+                @Override
+                public void componentResized(ComponentEvent e) {
+
+                    onComponentResized();
+                }
+            });
+        }
+        else {
+
+            canvas.setSize(appWidth, appHeight);
+            pack();
+        }
+        
+        setTitle(appTitle);
+        setupInput(canvas);
+        setVisible(true);
+        createBufferStrategy(canvas);
+        canvas.requestFocus();
+    }
+
+    protected void onComponentResized(ComponentEvent e) {
+
+        Dimension size = getContentPane().getSize();
+        screenCoordinates(size.width, size.height);
+        canvas.setLocation(vx, vy);
+        canvas.setSize(vw, vh);
+    }
+
+    @Override
+    protected void renderFrame(Graphics g) {
+
+        g.clearRect(0, 0, getScreenWidth(), getScreenHeight());
+    }
+
+    @Override
+    public int getScreenWidth() {
+
+        return canvas.getWidth();
+    }
+
+    @Override
+    public int getScreenHeight() {
+
+        return canvas.getHeight();
+    }
+}
