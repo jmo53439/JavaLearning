828d8b0b0554f171b93a81fd1f407420e54b481d
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-04 11:10:47 -0600

    Create StateController.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/gameusingeverything/state/StateController.java
@@ -0,0 +1,73 @@
+package com.jmlearning.randomthings.gamingprogramming.gameusingeverything.state;
+
+import com.jmlearning.randomthings.gamingprogramming.utils.Matrix3x3f;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class StateController {
+
+    private Map<String, Object> attributes;
+    private State currentState;
+
+    public StateController() {
+
+        attributes = Collections.synchronizedMap(new HashMap <>());
+    }
+
+    public void setState(State newState) {
+
+        if(currentState != null) {
+
+            currentState.exit();
+        }
+
+        if(newState != null) {
+
+            newState.setController(this);
+            newState.enter();
+        }
+
+        currentState = newState;
+    }
+
+    public void processInput(float delta) {
+
+        currentState.processInput(delta);
+    }
+
+    public void updateObjects(float delta) {
+
+        currentState.updateObjects(delta);
+    }
+
+    public void render(Graphics2D g, Matrix3x3f view) {
+
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        currentState.render(g, view);
+    }
+
+    public Object getAttribute(String name) {
+
+        return attributes.get(name);
+    }
+
+    public Object removeAttribute(String name) {
+
+        return attributes.remove(name);
+    }
+
+    public void setAttribute(String name, Object attribute) {
+
+        attributes.put(name, attribute);
+    }
+
+    public Set<String> getAttributeNames() {
+
+        return attributes.keySet();
+    }
+}
