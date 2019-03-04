90fc7732590cabcc881b38a9f99db5f7968fd1df
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-04 11:10:29 -0600

    Create State.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/gameusingeverything/state/State.java
@@ -0,0 +1,43 @@
+package com.jmlearning.randomthings.gamingprogramming.gameusingeverything.state;
+
+import com.jmlearning.randomthings.gamingprogramming.gameusingeverything.FinishedProduct;
+import com.jmlearning.randomthings.gamingprogramming.utils.Matrix3x3f;
+
+import java.awt.*;
+
+public class State {
+
+    protected StateController controller;
+    protected FinishedProduct game;
+
+    protected StateController getController() {
+
+        return controller;
+    }
+
+    public void setController(StateController controller) {
+
+        this.controller = controller;
+        game = (FinishedProduct) controller.getAttribute("game");
+    }
+
+    public void enter() {
+
+    }
+
+    public void processInput(float delta) {
+
+    }
+
+    public void updateObjects(float delta) {
+
+    }
+
+    public void render(Graphics2D g, Matrix3x3f view) {
+
+    }
+
+    public void exit() {
+        
+    }
+}
