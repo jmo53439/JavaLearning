1a563a41dea303e255d3a2fb7ad99b97f76d1c6a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-08 11:45:16 -0600

    Create LevelStarting.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/gameprogramming/gameusingeverything/state/LevelStarting.java
@@ -0,0 +1,56 @@
+package com.jmlearning.randomthings.gamingprogramming.gameusingeverything.state;
+
+import com.jmlearning.randomthings.gamingprogramming.gameusingeverything.admin.Acme;
+import com.jmlearning.randomthings.gamingprogramming.utils.Matrix3x3f;
+import com.jmlearning.randomthings.gamingprogramming.utils.Sprite;
+import com.jmlearning.randomthings.gamingprogramming.utils.Utility;
+
+import java.awt.*;
+
+public class LevelStarting extends State {
+
+    double time;
+    private Sprite background;
+    private GameState state;
+    private Acme acme;
+
+    public LevelStarting(GameState state) {
+
+        this.state = state;
+    }
+
+    @Override
+    public void enter() {
+
+        background = (Sprite) controller.getAttribute("background");
+        acme = (Acme) controller.getAttribute("ACME");
+        time = 0.0f;
+    }
+
+    @Override
+    public void updateObjects(float delta) {
+
+        time += delta;
+
+        if(time > 2.0) {
+
+            getController().setState(new LevelPlaying(state));
+        }
+    }
+
+    @Override
+    public void render(Graphics2D g, Matrix3x3f view) {
+
+        super.render(g, view);
+
+        background.render(g, view);
+        acme.drawScore(g, state.getScore());
+        acme.drawLives(g, view, state.getLives());
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.setColor(Color.GREEN);
+        Utility.drawCenteredString(g, game.getScreenWidth(), game.getScreenHeight() / 3,
+                "L E V E L " + state.getLevel());
+    }
+}
