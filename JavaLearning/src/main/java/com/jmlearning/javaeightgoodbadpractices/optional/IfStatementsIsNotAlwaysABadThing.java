688a837a7cbb86154b169fca0f36b9ef786db251
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-13 08:59:36 -0500

    Create IfStatementsIsNotAlwaysABadThing.java

---
+++ JavaLearning/src/main/java/com/jmlearning/javaeightgoodbadpractices/optional/IfStatementsIsNotAlwaysABadThing.java
@@ -0,0 +1,45 @@
+package com.jmlearning.javaeightgoodbadpractices.optional;
+
+import com.jmlearning.javaeightgoodbadpractices.Annotations.Good;
+import com.jmlearning.javaeightgoodbadpractices.Annotations.Ugly;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class IfStatementIsNotAlwaysABadThing {
+    
+    @Ugly
+    class CombiningSomeOptionals {
+        
+        public Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            
+            return Stream.of(first, second)
+                    .filter(Optional :: isPresent)
+                    .map(Optional :: get)
+                    .reduce(Integer :: sum);
+        }
+    }
+    
+    @Ugly
+    class CleverMapGame {
+        
+        public Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            
+            return first.map(b -> second.map(a -> b + a).orElse(b)).map(Optional :: of).orElse(second);
+        }
+    }
+    
+    @Good
+    class ClearAndConcise {
+        
+        public Optional<Integer> sum(Optional<Integer> first, Optional<Integer> second) {
+            
+            if(!first.isPresent() && !second.isPresent()) {
+                
+                return Optional.empty();
+            }
+            
+            return Optional.of(first.orElse(0) + second.orElse(0));
+        }
+    }
+}
