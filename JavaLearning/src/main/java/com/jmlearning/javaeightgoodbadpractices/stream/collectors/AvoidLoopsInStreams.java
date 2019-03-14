7e333facbc22bae6c84506fae11cbc41963eb95c
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-03-14 10:13:58 -0500

    Create AvoidLoopsInStreams.java

---
+++ JavaLearning/src/main/java/com/jmlearning/javaeightgoodbadpractices/stream/collectors/AvoidLoopsInStreams.java
@@ -0,0 +1,42 @@
+package com.jmlearning.javaeightgoodbadpractices.stream.collectors;
+
+import com.jmlearning.javaeightgoodbadpractices.Annotations;
+import com.jmlearning.javaeightgoodbadpractices.Annotations.Good;
+import com.jmlearning.javaeightgoodbadpractices.Annotations.Ugly;
+import com.jmlearning.javaeightgoodbadpractices.User;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AvoidLoopsInStreams {
+ 
+    private final Set<User> users = new HashSet <>();
+    
+    @Ugly
+    class UseExternalCounter {
+        
+        public double countAverageRolesPerUser() {
+            
+            if(users.isEmpty())
+                return 0;
+    
+            AtomicInteger totalCount = new AtomicInteger();
+            users.forEach(u -> totalCount.addAndGet(u.getRoles().size()));
+            
+            return totalCount.doubleValue() / users.size();
+        }
+    }
+    
+    @Good
+    class ApplyMappingsToTargetType {
+        
+        public double countAverageRolesPerUser() {
+            
+            return users.stream()
+                    .mapToDouble(u -> u.getRoles().size())
+                    .average()
+                    .orElse(0);
+        }
+    }
+}
