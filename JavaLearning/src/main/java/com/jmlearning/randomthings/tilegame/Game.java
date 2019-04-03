4b6645002e2de9c618fd9fb1e5c75bfdcb1fd32a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-03 09:30:15 -0500

    Create Game.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Game.java
@@ -0,0 +1,16 @@
+package com.jmlearning.randomthings.tilegame;
+
+import com.jmlearning.randomthings.tilegame.display.Display;
+
+public class Game {
+    
+    private Display display;
+    public int width, height;
+    
+    public Game(String title, int width, int height) {
+        
+        this.width = width;
+        this.height = height;
+        display = new Display(title, width, height);
+    }
+}
