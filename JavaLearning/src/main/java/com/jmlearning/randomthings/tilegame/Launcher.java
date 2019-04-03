bd8c4bfb64397886f3ca295da9c4a35fb4d3b790
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-03 08:58:21 -0500

    Create Launcher.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Launcher.java
@@ -0,0 +1,11 @@
+package com.jmlearning.randomthings.tilegame;
+
+import com.jmlearning.randomthings.tilegame.display.Display;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        
+        new Display("Meow!", 300, 300);
+    }
+}
