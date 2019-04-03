f1c94dfa86313e8159e64af5f7da3471a9667bd0
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-03 09:42:12 -0500

    Update Game.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Game.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Game.java
@@ -2,15 +2,70 @@ package com.jmlearning.randomthings.tilegame;
 
 import com.jmlearning.randomthings.tilegame.display.Display;
 
-public class Game {
+public class Game implements Runnable {
     
     private Display display;
     public int width, height;
+    public String title;
+    private boolean running = false;
+    private Thread thread;
     
     public Game(String title, int width, int height) {
         
         this.width = width;
         this.height = height;
+        this.title = title;
+    }
+    
+    @Override
+    public void run() {
+    
+        init();
+        
+        while(running) {
+        
+            tick();
+            render();
+        }
+    }
+    
+    private void init() {
+      
         display = new Display(title, width, height);
+    }
+    
+    private void tick() {
+    
+    }
+    
+    private void render() {
+    
+    }
+    
+    public synchronized void start() {
+        
+        if(running)
+            return;
+        
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+    
+    public synchronized void stop() {
+        
+        if(!running)
+            return;
+        
+        running = false;
+        
+        try {
+            
+            thread.join();
+        }
+        catch(InterruptedException e) {
+            
+            e.printStackTrace();
+        }
     }
 }
