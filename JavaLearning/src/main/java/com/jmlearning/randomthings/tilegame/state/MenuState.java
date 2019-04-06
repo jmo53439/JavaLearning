8a0b8bb067c223d7ed9e04f1355d46547796425b
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-06 11:24:40 -0500

    Create MenuState.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/state/MenuState.java
@@ -0,0 +1,20 @@
+package com.jmlearning.randomthings.tilegame.states;
+
+import java.awt.*;
+
+public class MenuState extends State {
+    
+    public MenuState() {
+    
+    }
+    
+    @Override
+    public void tick() {
+    
+    }
+    
+    @Override
+    public void render(Graphics g) {
+    
+    }
+}
