8e05e5adb677fadababe837b9713e133547c204f
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-06 12:15:59 -0500

    Update Player.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
@@ -11,24 +11,33 @@ public class Player extends Creature {
     
     public Player(Game game, float x, float y) {
         
-        super(x, y);
+        super(x, y, DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT);
         this.game = game;
     }
     
     @Override
     public void tick() {
     
+        getInput();
+        move();
+    }
+    
+    private void getInput() {
+        
+        xMove = 0;
+        yMove = 0;
+        
         if(game.getKeyManager().up)
-            y -= 3;
+            yMove = -speed;
         
         if(game.getKeyManager().down)
-            y += 3;
+            yMove = speed;
         
         if(game.getKeyManager().left)
-            x -= 3;
+            xMove = -speed;
         
         if(game.getKeyManager().right)
-            x += 3;
+            xMove = speed;
     }
     
     @Override
