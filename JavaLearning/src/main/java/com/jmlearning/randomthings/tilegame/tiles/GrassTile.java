11c3f171b08ff10c6d4ff3357bcab3e58bd6db83
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-06 12:31:39 -0500

    Create GrassTile.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/tiles/GrassTile.java
@@ -0,0 +1,11 @@
+package com.jmlearning.randomthings.tilegame.tiles;
+
+import com.jmlearning.randomthings.tilegame.gfx.Assets;
+
+public class GrassTile extends Tile {
+    
+    public GrassTile(int id) {
+        
+        super(Assets.grass, id);
+    }
+}
