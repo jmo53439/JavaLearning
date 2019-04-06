724a3efb6cb7f780bb760ea62a826258eeb4001a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-06 12:30:25 -0500

    Create DirtTile.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/tiles/DirtTile.java
@@ -0,0 +1,11 @@
+package com.jmlearning.randomthings.tilegame.tiles;
+
+import com.jmlearning.randomthings.tilegame.gfx.Assets;
+
+public class DirtTile extends Tile {
+    
+    public DirtTile(int id) {
+        
+        super(Assets.dirt, id);
+    }
+}
