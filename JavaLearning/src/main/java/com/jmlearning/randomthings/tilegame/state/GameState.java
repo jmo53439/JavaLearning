cb8520afb6cedff120977a365ce844504ab335a9
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-06 12:43:53 -0500

    Update GameState.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/state/GameState.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/state/GameState.java
@@ -2,28 +2,33 @@ package com.jmlearning.randomthings.tilegame.states;
 
 import com.jmlearning.randomthings.tilegame.Game;
 import com.jmlearning.randomthings.tilegame.entities.creatures.Player;
+import com.jmlearning.randomthings.tilegame.worlds.World;
 
 import java.awt.*;
 
 public class GameState extends State {
     
     private Player player;
+    private World world;
     
     public GameState(Game game) {
     
         super(game);
         player = new Player(game, 100, 100);
+        world = new World("");
     }
     
     @Override
     public void tick() {
     
+        world.tick();
         player.tick();
     }
     
     @Override
     public void render(Graphics g) {
     
+        world.render(g);
         player.render(g);
     }
 }
