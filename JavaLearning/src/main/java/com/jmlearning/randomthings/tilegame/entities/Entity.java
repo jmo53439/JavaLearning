95d20b7184055c1296a288f8d6a623445ad93b5c
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-08 11:31:14 -0500

    Update Entity.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/Entity.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/Entity.java
@@ -1,14 +1,18 @@
 package com.jmlearning.randomthings.tilegame.entities;
 
+import com.jmlearning.randomthings.tilegame.Game;
+
 import java.awt.*;
 
 public abstract class Entity {
     
+    protected Game game;
     protected float x, y;
     protected int width, height;
     
-    public Entity(float x, float y, int width, int height) {
+    public Entity(Game game, float x, float y, int width, int height) {
         
+        this.game = game;
         this.x = x;
         this.y = y;
         this.width = width;
