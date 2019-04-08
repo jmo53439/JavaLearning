5381b3742666e7fe92e65891e27c34f81c99ecf0
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-08 09:08:47 -0500

    Update World.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/worlds/World.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/worlds/World.java
@@ -1,12 +1,14 @@
 package com.jmlearning.randomthings.tilegame.worlds;
 
 import com.jmlearning.randomthings.tilegame.tiles.Tile;
+import com.jmlearning.randomthings.tilegame.utils.Utils;
 
 import java.awt.*;
 
 public class World {
     
     private int width, height;
+    private int spawnX, spawnY;
     private int[][] tiles;
     
     public World(String path) {
@@ -29,7 +31,7 @@ public class World {
         }
     }
     
-    private Tile getTile(int x, int y) {
+    public Tile getTile(int x, int y) {
         
         Tile t = Tile.tiles[tiles[x][y]];
         
@@ -41,5 +43,20 @@ public class World {
     
     private void loadWorld(String path) {
         
+        String file = Utils.loadFileAsString(path);
+        String[] tokens = file.split("\\s+");
+        width = Utils.parseInt(tokens[0]);
+        height = Utils.parseInt(tokens[1]);
+        spawnX = Utils.parseInt(tokens[2]);
+        spawnY = Utils.parseInt(tokens[3]);
+        tiles = new int[width][height];
+        
+        for(int y = 0; y < height; y++) {
+            
+            for(int x = 0; x < width; x++) {
+            
+                tiles[x][y] = Utils.parseInt(tokens[(x + y * width) + 4]);
+            }
+        }
     }
 }
