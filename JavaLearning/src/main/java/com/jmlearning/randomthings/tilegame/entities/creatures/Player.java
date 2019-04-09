29f71868e319a4ab519b6e22fbdefa14c48331ef
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-09 09:01:59 -0500

    Update Player.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
@@ -1,15 +1,15 @@
 package com.jmlearning.randomthings.tilegame.entities.creatures;
 
-import com.jmlearning.randomthings.tilegame.Game;
+import com.jmlearning.randomthings.tilegame.Handler;
 import com.jmlearning.randomthings.tilegame.gfx.Assets;
 
 import java.awt.*;
 
 public class Player extends Creature {
     
-    public Player(Game game, float x, float y) {
+    public Player(Handler handler, float x, float y) {
         
-        super(game, x, y, DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT);
+        super(handler, x, y, DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT);
     }
     
     @Override
@@ -17,7 +17,7 @@ public class Player extends Creature {
     
         getInput();
         move();
-        game.getGameCamera().centerOnEntity(this);
+        handler.getGameCamera().centerOnEntity(this);
     }
     
     private void getInput() {
@@ -25,24 +25,24 @@ public class Player extends Creature {
         xMove = 0;
         yMove = 0;
         
-        if(game.getKeyManager().up)
+        if(handler.getKeyManager().up)
             yMove = -speed;
         
-        if(game.getKeyManager().down)
+        if(handler.getKeyManager().down)
             yMove = speed;
         
-        if(game.getKeyManager().left)
+        if(handler.getKeyManager().left)
             xMove = -speed;
         
-        if(game.getKeyManager().right)
+        if(handler.getKeyManager().right)
             xMove = speed;
     }
     
     @Override
     public void render(Graphics g) {
     
-        g.drawImage(Assets.player, (int)(x - game.getGameCamera().getxOffset()),
-                (int)(y - game.getGameCamera().getyOffset()),
+        g.drawImage(Assets.player, (int)(x - handler.getGameCamera().getxOffset()),
+                (int)(y - handler.getGameCamera().getyOffset()),
                 width, height, null);
     }
 }
