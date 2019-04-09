738f4417918358b86d82d0206457162b2af3b90a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-09 09:17:05 -0500

    Update Launcher.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Launcher.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/Launcher.java
@@ -4,7 +4,7 @@ public class Launcher {
     
     public static void main(String[] args) {
         
-        Game game = new Game("Meow", 640, 360);
+        Game game = new Game("Meow", 640, 480);
         game.start();
     }
 }
