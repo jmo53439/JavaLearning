c2ee4361c7b5812fead0b05f8db53cd1ddb6db58
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-10 11:39:13 -0500

    Update Player.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/entities/creatures/Player.java
@@ -1,11 +1,15 @@
 package com.jmlearning.randomthings.tilegame.entities.creatures;
 
 import com.jmlearning.randomthings.tilegame.Handler;
+import com.jmlearning.randomthings.tilegame.gfx.Animation;
 import com.jmlearning.randomthings.tilegame.gfx.Assets;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Player extends Creature {
+    
+    private Animation animateUp, animateDown, animateLeft, animateRight;
     
     public Player(Handler handler, float x, float y) {
         
@@ -15,14 +19,26 @@ public class Player extends Creature {
         bounds.y = 44;
         bounds.width = 19;
         bounds.height = 19;
+        
+        animateUp = new Animation(500, Assets.playerUp);
+        animateDown = new Animation(500, Assets.playerDown);
+        animateLeft = new Animation(500, Assets.playerLeft);
+        animateRight = new Animation(500, Assets.playerRight);
     }
     
     @Override
     public void tick() {
     
+        // movement
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
+        
+        // animations
+        animateUp.tick();
+        animateDown.tick();
+        animateLeft.tick();
+        animateRight.tick();
     }
     
     private void getInput() {
@@ -46,12 +62,34 @@ public class Player extends Creature {
     @Override
     public void render(Graphics g) {
     
-        g.drawImage(Assets.player, (int)(x - handler.getGameCamera().getxOffset()),
+        g.drawImage(getCurrentAnimationFrame(),
+                (int)(x - handler.getGameCamera().getxOffset()),
                 (int)(y - handler.getGameCamera().getyOffset()),
                 width, height, null);
+                
 //        g.setColor(Color.RED);
 //        g.fillRect((int)(x + bounds.x - handler.getGameCamera().getxOffset()),
 //                (int)(y + bounds.y - handler.getGameCamera().getyOffset()),
 //                bounds.width, bounds.height);
+    }
+    
+    private BufferedImage getCurrentAnimationFrame() {
+        
+        if(xMove < 0) {
+            
+            return animateLeft.getCurrentFrame();
+        }
+        else if(xMove > 0) {
+            
+            return animateRight.getCurrentFrame();
+        }
+        else if(yMove < 0) {
+            
+            return animateUp.getCurrentFrame();
+        }
+        else {
+            
+            return animateDown.getCurrentFrame();
+        }
     }
 }
