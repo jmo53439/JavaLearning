1243b0f0b3fd868400b59482273d7a02ff26396e
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-10 08:40:44 -0500

    Update World.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/worlds/World.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/worlds/World.java
@@ -86,4 +86,14 @@ public class World {
             }
         }
     }
+    
+    public int getWidth() {
+        
+        return width;
+    }
+    
+    public int getHeight() {
+        
+        return height;
+    }
 }
