8c5b577b9c981c20b473a776b39f2305406d5a81
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-10 08:41:09 -0500

    Update GameCamera.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/GameCamera.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/GameCamera.java
@@ -1,30 +1,56 @@
 package com.jmlearning.randomthings.tilegame.gfx;
 
-import com.jmlearning.randomthings.tilegame.Game;
+import com.jmlearning.randomthings.tilegame.Handler;
 import com.jmlearning.randomthings.tilegame.entities.Entity;
+
+import static com.jmlearning.randomthings.tilegame.tiles.Tile.TILE_HEIGHT;
+import static com.jmlearning.randomthings.tilegame.tiles.Tile.TILE_WIDTH;
 
 public class GameCamera {
     
-    private Game game;
+    private Handler handler;
     private float xOffset, yOffset;
     
-    public GameCamera(Game game, float xOffset, float yOffset) {
+    public GameCamera(Handler handler, float xOffset, float yOffset) {
         
-        this.game = game;
+        this.handler = handler;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
     }
     
     public void centerOnEntity(Entity e) {
         
-        xOffset = e.getX() - game.getWidth() / 2 + e.getWidth() / 2;
-        yOffset = e.getY() - game.getHeight() / 2 + e.getHeight() / 2;
+        xOffset = e.getX() - handler.getWidth() / 2 + e.getWidth() / 2;
+        yOffset = e.getY() - handler.getHeight() / 2 + e.getHeight() / 2;
+        checkBlankSpace();
     }
     
     public void move(float xAmt, float yAmt) {
         
         xOffset += xAmt;
         yOffset += yAmt;
+        checkBlankSpace();
+    }
+    
+    public void checkBlankSpace() {
+        
+        if(xOffset < 0) {
+        
+            xOffset = 0;
+        }
+        else if(xOffset > handler.getWorld().getWidth() * TILE_WIDTH - handler.getWidth()) {
+            
+            xOffset = handler.getWorld().getWidth() * TILE_WIDTH - handler.getWidth();
+        }
+        
+        if(yOffset < 0) {
+            
+            yOffset = 0;
+        }
+        else if(yOffset > handler.getWorld().getHeight() * TILE_HEIGHT - handler.getHeight()) {
+            
+            yOffset = handler.getWorld().getHeight() * TILE_HEIGHT - handler.getHeight();
+        }
     }
     
     public float getxOffset() {
