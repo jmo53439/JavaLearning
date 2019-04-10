acba677618c0c94183a98dcc5c69c57991f1722f
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-10 12:30:00 -0500

    Update Assets.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/Assets.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/Assets.java
@@ -7,6 +7,7 @@ public class Assets {
     private static final int width = 32, height = 32;
     public static BufferedImage player, dirt, grass, stone, tree;
     public static BufferedImage[] playerUp, playerDown, playerLeft, playerRight;
+    public static BufferedImage[] zombieUp, zombieDown, zombieLeft, zombieRight;
     
     public static void init() {
         
@@ -33,5 +34,22 @@ public class Assets {
         
         playerRight[0] = sheet.crop(width * 4, height, width, height);
         playerRight[1] = sheet.crop(width * 5, height, width, height);
+        
+        zombieUp = new BufferedImage[2];
+        zombieDown = new BufferedImage[2];
+        zombieLeft = new BufferedImage[2];
+        zombieRight = new BufferedImage[2];
+        
+        zombieUp[0] = sheet.crop(width * 6, height * 2, width, height);
+        zombieUp[1] = sheet.crop(width * 7, height * 2, width, height);
+        
+        zombieDown[0] = sheet.crop(width * 4, height * 2, width, height);
+        zombieDown[1] = sheet.crop(width * 5, height * 2, width, height);
+        
+        zombieLeft[0] = sheet.crop(width * 6, height * 3, width, height);
+        zombieLeft[1] = sheet.crop(width * 7, height * 3, width, height);
+        
+        zombieRight[0] = sheet.crop(width * 4, height * 3, width, height);
+        zombieRight[1] = sheet.crop(width * 5, height * 3, width, height);
     }
 }
