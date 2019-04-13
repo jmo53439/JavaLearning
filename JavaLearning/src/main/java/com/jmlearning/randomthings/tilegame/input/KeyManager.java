6c6d976c823ff7bbc305e99693aa1eeb60a8c3c5
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-13 10:36:21 -0500

    Update KeyManager.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/input/KeyManager.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/input/KeyManager.java
@@ -5,16 +5,36 @@ import java.awt.event.KeyListener;
 
 public class KeyManager implements KeyListener {
     
-    private boolean[] keys;
+    private boolean[] keys, justPressed, cantPress;
     public boolean up, down, left, right;
     public boolean aUp, aDown, aLeft, aRight;
     
     public KeyManager() {
         
         keys = new boolean[256];
+        justPressed = new boolean[keys.length];
+        cantPress = new boolean[keys.length];
     }
     
     public void tick() {
+        
+        for(int i = 0; i < keys.length; i++) {
+            
+            if(cantPress[i] && !keys[i]) {
+                
+                cantPress[i] = false;
+            }
+            else if(justPressed[i]) {
+                
+                cantPress[i] = true;
+                justPressed[i] = false;
+            }
+            
+            if(!cantPress[i] && keys[i]) {
+                
+                justPressed[i] = true;
+            }
+        }
         
         up = keys[KeyEvent.VK_W];
         down = keys[KeyEvent.VK_S];
@@ -25,6 +45,14 @@ public class KeyManager implements KeyListener {
         aDown = keys[KeyEvent.VK_DOWN];
         aLeft = keys[KeyEvent.VK_LEFT];
         aRight = keys[KeyEvent.VK_RIGHT];
+    }
+    
+    public boolean keyJustPressed(int keyCode) {
+        
+        if(keyCode < 0 || keyCode >= keys.length)
+            return false;
+        
+        return justPressed[keyCode];
     }
     
     @Override
