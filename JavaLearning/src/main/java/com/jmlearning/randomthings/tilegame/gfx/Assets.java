fbc96d78833d430b4b833bf95c6d281b1ab6f334
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-13 09:27:28 -0500

    Update Assets.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/Assets.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/gfx/Assets.java
@@ -5,7 +5,7 @@ import java.awt.image.BufferedImage;
 public class Assets {
     
     private static final int width = 32, height = 32;
-    public static BufferedImage player, dirt, grass, stone, tree;
+    public static BufferedImage player, dirt, grass, stone, tree, rock;
     public static BufferedImage[] playerUp, playerDown, playerLeft, playerRight;
     public static BufferedImage[] zombieUp, zombieDown, zombieLeft, zombieRight;
     public static BufferedImage[] btnStart;
@@ -14,12 +14,6 @@ public class Assets {
         
         SpriteSheet sheet = new SpriteSheet(ImageLoader.loadImage("/textures/sheet.png"));
         
-        player = sheet.crop(width * 4, 0, width, height);
-        dirt = sheet.crop(width, 0, width, height);
-        grass = sheet.crop(width * 2, 0, width, height);
-        stone = sheet.crop(width * 3, 0, width, height);
-        tree = sheet.crop(0, 0, width, height);
-    
         btnStart = new BufferedImage[2];
         btnStart[0] = sheet.crop(width * 6, height * 4, width * 2, height);
         btnStart[1] = sheet.crop(width * 6, height * 5, width * 2, height);
@@ -31,13 +25,10 @@ public class Assets {
         
         playerUp[0] = sheet.crop(width * 6, 0, width, height);
         playerUp[1] = sheet.crop(width * 7, 0, width, height);
-        
         playerDown[0] = sheet.crop(width * 4, 0, width, height);
         playerDown[1] = sheet.crop(width * 5, 0, width, height);
-        
         playerLeft[0] = sheet.crop(width * 6, height, width, height);
         playerLeft[1] = sheet.crop(width * 7, height, width, height);
-        
         playerRight[0] = sheet.crop(width * 4, height, width, height);
         playerRight[1] = sheet.crop(width * 5, height, width, height);
         
@@ -48,14 +39,18 @@ public class Assets {
         
         zombieUp[0] = sheet.crop(width * 6, height * 2, width, height);
         zombieUp[1] = sheet.crop(width * 7, height * 2, width, height);
-        
         zombieDown[0] = sheet.crop(width * 4, height * 2, width, height);
         zombieDown[1] = sheet.crop(width * 5, height * 2, width, height);
-        
         zombieLeft[0] = sheet.crop(width * 6, height * 3, width, height);
         zombieLeft[1] = sheet.crop(width * 7, height * 3, width, height);
-        
         zombieRight[0] = sheet.crop(width * 4, height * 3, width, height);
         zombieRight[1] = sheet.crop(width * 5, height * 3, width, height);
+    
+        player = sheet.crop(width * 4, 0, width, height);
+        dirt = sheet.crop(width, 0, width, height);
+        grass = sheet.crop(width * 2, 0, width, height);
+        stone = sheet.crop(width * 3, 0, width, height);
+        tree = sheet.crop(0, 0, width, height);
+        rock = sheet.crop(0, height * 2, width, height);
     }
 }
