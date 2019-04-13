35cbc96c168217e8295bc96f5e37d87a40643239
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-13 08:50:19 -0500

    Update MenuState.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/state/MenuState.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/state/MenuState.java
@@ -1,31 +1,43 @@
 package com.jmlearning.randomthings.tilegame.states;
 
 import com.jmlearning.randomthings.tilegame.Handler;
+import com.jmlearning.randomthings.tilegame.gfx.Assets;
+import com.jmlearning.randomthings.tilegame.ui.ClickListener;
+import com.jmlearning.randomthings.tilegame.ui.UIImageButton;
+import com.jmlearning.randomthings.tilegame.ui.UIManager;
 
 import java.awt.*;
 
 public class MenuState extends State {
     
+    private UIManager uiManager;
+    
     public MenuState(Handler handler) {
     
         super(handler);
+        uiManager = new UIManager(handler);
+        handler.getMouseManager().setUIManager(uiManager);
+        
+        uiManager.addObject(new UIImageButton(200, 200, 128, 64, Assets.btnStart, new ClickListener() {
+    
+            @Override
+            public void onClick() {
+        
+                handler.getMouseManager().setUIManager(null);
+                State.setState(handler.getGame().gameState);
+            }
+        }));
     }
     
     @Override
     public void tick() {
     
-        if(handler.getMouseManager().isLeftPressed() &&
-                handler.getMouseManager().isRightPressed()) {
-            
-            State.setState(handler.getGame().gameState);
-        }
+        uiManager.tick();
     }
     
     @Override
     public void render(Graphics g) {
     
-        g.setColor(Color.RED);
-        g.fillRect(handler.getMouseManager().getMouseX(),
-                handler.getMouseManager().getMouseY(), 8, 8);
+        uiManager.render(g);
     }
 }
