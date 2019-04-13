e82d0bff6da69ec1bf0cc64c8947551a16f5db2c
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-13 10:38:21 -0500

    Create Inventory.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/inventory/Inventory.java
@@ -0,0 +1,61 @@
+package com.jmlearning.randomthings.tilegame.inventory;
+
+import com.jmlearning.randomthings.tilegame.Handler;
+import com.jmlearning.randomthings.tilegame.items.Item;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class Inventory {
+    
+    private Handler handler;
+    private boolean active = false;
+    private ArrayList<Item> inventoryItems;
+    
+    public Inventory(Handler handler) {
+        
+        this.handler = handler;
+        inventoryItems = new ArrayList <>();
+    }
+    
+    public void tick() {
+        
+        if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_E))
+            active = !active;
+        
+        if(!active)
+            return;
+    }
+    
+    public void render(Graphics g) {
+        
+        if(!active)
+            return;
+    }
+    
+    public void addItem(Item item) {
+        
+        for(Item i : inventoryItems) {
+            
+            if(i.getId() == item.getId()) {
+                
+                i.setCount(i.getCount() + item.getCount());
+                
+                return;
+            }
+        }
+        
+        inventoryItems.add(item);
+    }
+    
+    public Handler getHandler() {
+        
+        return handler;
+    }
+    
+    public void setHandler(Handler handler) {
+        
+        this.handler = handler;
+    }
+}
