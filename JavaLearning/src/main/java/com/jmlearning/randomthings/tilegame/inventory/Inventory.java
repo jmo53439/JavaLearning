7a531460933dc795bbf42b6c863634942a2c64af
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-15 08:56:41 -0500

    Update Inventory.java

--- JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/inventory/Inventory.java
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/tilegame/inventory/Inventory.java
@@ -1,6 +1,8 @@
 package com.jmlearning.randomthings.tilegame.inventory;
 
 import com.jmlearning.randomthings.tilegame.Handler;
+import com.jmlearning.randomthings.tilegame.gfx.Assets;
+import com.jmlearning.randomthings.tilegame.gfx.Text;
 import com.jmlearning.randomthings.tilegame.items.Item;
 
 import java.awt.*;
@@ -12,6 +14,15 @@ public class Inventory {
     private Handler handler;
     private boolean active = false;
     private ArrayList<Item> inventoryItems;
+    private int invX = 64, invY = 48,
+            invWidth = 512, invHeight = 384,
+            invListCenterX = invX + 171,
+            invListCenterY = invY + invHeight / 2 + 5,
+            invListSpacing = 30;
+    private int invImageX = 452, invImageY = 82,
+            invImageWidth = 64, invImageHeight = 64;
+    private int invCountX = 484, invCountY = 172;
+    private int selectedItem = 0;
     
     public Inventory(Handler handler) {
         
@@ -26,12 +37,61 @@ public class Inventory {
         
         if(!active)
             return;
+        
+        if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_W))
+            selectedItem--;
+        
+        if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_S))
+            selectedItem++;
+        
+        if(selectedItem < 0) {
+            
+            selectedItem = inventoryItems.size() - 1;
+        }
+        else if(selectedItem >= inventoryItems.size()) {
+            
+            selectedItem = 0;
+        }
     }
     
     public void render(Graphics g) {
         
         if(!active)
             return;
+        
+        g.drawImage(Assets.inventoryScreen, invX, invY, invWidth, invHeight, null);
+        
+        int len = inventoryItems.size();
+        
+        if(len == 0)
+            return;
+        
+        for(int i = -5; i < 6; i++) {
+            
+            if(selectedItem + i < 0 || selectedItem + i >= len) {
+                
+                continue;
+            }
+            
+            if(i == 0) {
+    
+                Text.drawString(g, "> " + inventoryItems.get(selectedItem + i).getName() +
+                        " <", invListCenterX, invListCenterY + i * invListSpacing,
+                        true, Color.YELLOW, Assets.font28);
+            }
+            else {
+                
+                Text.drawString(g, inventoryItems.get(selectedItem + i).getName(),
+                        invListCenterX, invListCenterY + i * invListSpacing,
+                        true, Color.WHITE, Assets.font28);
+            }
+        }
+        
+        Item item = inventoryItems.get(selectedItem);
+        g.drawImage(item.getTexture(), invImageX, invImageY,
+                invImageWidth, invImageHeight, null);
+        Text.drawString(g, Integer.toString(item.getCount()),
+                invCountX, invCountY, true, Color.WHITE, Assets.font28);
     }
     
     public void addItem(Item item) {
@@ -57,5 +117,10 @@ public class Inventory {
     public void setHandler(Handler handler) {
         
         this.handler = handler;
+    }
+    
+    public boolean isActive() {
+        
+        return active;
     }
 }
