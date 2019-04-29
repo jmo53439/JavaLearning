f1995c9c44533087d4d59dca2ebcd9c06e4bbfdd
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:37:33 -0500

    Create GoldArmor.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/armor/GoldArmor.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.armor;
+
+public class GoldArmor extends Armor {
+    
+    private String name = "gold armor";
+    private int durabilityPoints = 20;
+    private int dmgMitigated = 10;
+}
