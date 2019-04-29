d1853e805b621a3038a279b1bc4517af72365b36
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:39:35 -0500

    Create WoodSword.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/weapons/WoodSword.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.weapons;
+
+public class WoodSword extends Sword {
+    
+    private int durabilityPoints = 10;
+    private int dmgIncrease = 5;
+    private String name = "wood sword";
+}
