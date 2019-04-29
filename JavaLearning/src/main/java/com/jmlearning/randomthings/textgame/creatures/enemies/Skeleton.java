97169f391cfd856559e2d231171cabb2c415e8bc
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:32:57 -0500

    Create Skeleton.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/creatures/enemies/Skeleton.java
@@ -0,0 +1,24 @@
+package com.jmlearning.randomthings.textgame.creatures.enemies;
+
+public class Skeleton extends Enemy {
+    
+    public Skeleton() {
+        
+        super();
+        
+        this.setName(this.getClass().getSimpleName());
+        this.setHealth(RANDOM.nextInt(TIER2_MAX_HP - TIER2_MIN_HP) + TIER2_MIN_HP);
+    }
+    
+    @Override
+    public int atk() {
+        
+        return RANDOM.nextInt(TIER2_MAX_DMG);
+    }
+    
+    @Override
+    public void takeDmg(int dmg) {
+        
+        this.setHealth(this.getHealth() - dmg);
+    }
+}
