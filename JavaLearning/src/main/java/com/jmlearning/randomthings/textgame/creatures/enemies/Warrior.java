80d5e1596c9d9126ddb3f595c3cb04d83ea28a9d
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:33:56 -0500

    Create Warrior.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/creatures/enemies/Warrior.java
@@ -0,0 +1,24 @@
+package com.jmlearning.randomthings.textgame.creatures.enemies;
+
+public class Warrior extends Enemy {
+    
+    public Warrior() {
+        
+        super();
+        
+        this.setName(this.getClass().getSimpleName());
+        this.setHealth(RANDOM.nextInt(TIER3_MAX_HP - TIER3_MIN_HP) + TIER3_MIN_HP);
+    }
+    
+    @Override
+    public int atk() {
+        
+        return RANDOM.nextInt(TIER3_MAX_DMG);
+    }
+    
+    @Override
+    public void takeDmg(int dmg) {
+    
+        this.setHealth(this.getHealth() - dmg);
+    }
+}
