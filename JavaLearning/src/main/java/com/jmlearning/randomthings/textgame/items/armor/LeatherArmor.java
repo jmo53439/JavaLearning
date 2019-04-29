932e3951339d2ddfb72369a7efdbb84970d58532
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:37:59 -0500

    Create LeatherArmor.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/armor/LeatherArmor.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.armor;
+
+public class LeatherArmor extends Armor {
+    
+    private String name = "leather armor";
+    private int durabilityPoints = 5;
+    private int dmgMitigated = 2;
+}
