91812d5bc5a50f53b8d671c0d5a59ebf638b880a
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:39:11 -0500

    Create GoldSword.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/weapons/GoldSword.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.weapons;
+
+public class GoldSword extends Sword {
+    
+    private int durabilityPoints = 20;
+    private int dmgIncrease = 15;
+    private String name = "gold sword";
+}
