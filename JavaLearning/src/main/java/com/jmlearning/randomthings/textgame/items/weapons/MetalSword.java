53f01a610148a01f84742d58ae271cafd283d630
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:39:23 -0500

    Create MetalSword.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/weapons/MetalSword.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.weapons;
+
+public class MetalSword extends Sword {
+    
+    private int durabilityPoints = 15;
+    private int dmgIncrease = 10;
+    private String name = "metal sword";
+}
