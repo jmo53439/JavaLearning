5c802fad6969bbb1d64c40b64283ab9da551a75d
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:37:47 -0500

    Create IronArmor.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/items/armor/IronArmor.java
@@ -0,0 +1,8 @@
+package com.jmlearning.randomthings.textgame.items.armor;
+
+public class IronArmor extends Armor {
+    
+    private String name = "iron armor";
+    private int durabilityPoints = 10;
+    private int dmgMitigated = 5;
+}
