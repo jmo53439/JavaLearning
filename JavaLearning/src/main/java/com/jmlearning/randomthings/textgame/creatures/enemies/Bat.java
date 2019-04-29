10d57dc240088680672840641ac6add5786dbcda
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-04-29 08:29:26 -0500

    Create Bat.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/textgame/creatures/enemies/Bat.java
@@ -0,0 +1,24 @@
+package com.jmlearning.randomthings.textgame.creatures.enemies;
+
+public class Bat extends Enemy {
+    
+    public Bat() {
+        
+        super();
+        
+        this.setName(this.getClass().getSimpleName());
+        this.setHealth(RANDOM.nextInt(TIER1_MAX_HP - TIER1_MIN_HP) + TIER1_MIN_HP);
+    }
+    
+    @Override
+    public int atk() {
+        
+        return RANDOM.nextInt(TIER1_MAX_DMG);
+    }
+    
+    @Override
+    public void takeDmg(int dmg) {
+    
+        this.setHealth(this.getHealth() - dmg);
+    }
+}
