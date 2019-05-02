db132365fd69df1fde9e8b3c1dbf84db67687dce
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-05-02 10:54:59 -0500

    Create MyCardListener.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/uno/controllers/MyCardListener.java
@@ -0,0 +1,59 @@
+package com.jmlearning.randomthings.uno.controllers;
+
+import com.jmlearning.randomthings.uno.view.UNOCard;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MyCardListener extends MouseAdapter {
+    
+    UNOCard sourceCard;
+    Server myServer;
+    
+    public void setServer(Server server) {
+        
+        myServer = server;
+    }
+    
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+        sourceCard = (UNOCard) e.getSource();
+        
+        try {
+            
+            if(myServer.canPlay)
+                myServer.playThisCard(sourceCard);
+        }
+        catch(NullPointerException ex) {
+            
+            ex.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+        super.mouseEntered(e);
+        
+        sourceCard = (UNOCard) e.getSource();
+        Point p = sourceCard.getLocation();
+        p.y -= 20;
+        sourceCard.setLocation(p);
+    }
+    
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+        sourceCard = (UNOCard) e.getSource();
+        Point p = sourceCard.getLocation();
+        p.y += 20;
+        sourceCard.setLocation(p);
+    }
+    
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    
+    }
+}
