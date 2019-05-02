57a676a2b4c6563d7fa60434541dd91f03d41213
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-05-02 14:09:33 -0500

    Create Player.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/chess/game/Player.java
@@ -0,0 +1,9 @@
+package com.jmlearning.randomthings.chess.game;
+
+import com.jmlearning.randomthings.chess.pieces.Piece;
+
+public class Player {
+    
+    private String name;
+    private Piece.Color color;
+}
