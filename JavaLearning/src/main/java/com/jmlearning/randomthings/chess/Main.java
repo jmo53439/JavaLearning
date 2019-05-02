67bd90a775d924bc2500c3b989673830736b6c53
jmo53439 <41338955+jmo53439@users.noreply.github.com>
2019-05-02 14:05:54 -0500

    Create Main.java

---
+++ JavaLearning/src/main/java/com/jmlearning/randomthings/chess/Main.java
@@ -0,0 +1,11 @@
+package com.jmlearning.randomthings.chess;
+
+import com.jmlearning.randomthings.chess.game.Core;
+
+public class Main {
+   
+    public static void main(String[] args) {
+        
+        Core.launch();
+    }
+}
